977e7d6c25d8a467c60b715690dbd8ddb3b5d79a
yu2000324 <46483171+yu2000324@users.noreply.github.com>
2019-07-11 20:19:20 +0800

    用turtle库绘制一个棒棒糖

---
+++ rainbow.py
@@ -0,0 +1,33 @@
+import turtle
+size=15
+turtle.pensize(15)
+for i in range(7):
+    if i == 0:
+        turtle.color('purple')
+    if i == 1:
+        turtle.color('blue')
+    if i == 2:
+        turtle.color('cyan')
+    if i == 3:
+        turtle.color('green')
+    if i == 4:
+        turtle.color('yellow')
+    if i == 5:
+        turtle.color('orange')
+    if i == 6:
+        turtle.color('red')
+    turtle.circle(size)
+    size+=15
+    if i==6:
+        break
+    turtle.seth(-90)
+    turtle.penup()
+    turtle.fd(15)
+    turtle.pendown()
+    turtle.seth(360)
+turtle.fd(-3)
+turtle.seth(-90)
+turtle.color('purple')
+turtle.pensize(17)
+turtle.fd(200)
+turtle.done()
